9ea49206a686ac428f3a0d3551f988b91430d91d
Thomas Stanley <t.stanley@f5.com>
2021-06-03 09:15:13 -0700

    a bunch of new files for installing esp, registry, and camel

---
+++ camel_install/Sample.java
@@ -0,0 +1,9 @@
+import org.apache.camel.builder.RouteBuilder;
+
+public class Sample extends RouteBuilder {
+  @Override
+  public void configure() throws Exception {
+       from("timer:tick")
+        .log("Hello Camel K!");
+  }
+}
